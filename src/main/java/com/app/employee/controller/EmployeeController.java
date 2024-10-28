74aece5c39f3184538d27fcc321c68b0f2baac55
IssamBenhida <issam.benhida.911@gmail.com>
2024-10-28 09:23:13 +0000

    Creation de l'entité CRUD.

---
+++ src/main/java/com/app/employee/controller/EmployeeController.java
@@ -0,0 +1,47 @@
+package com.app.employee.controller;
+
+import com.app.employee.model.Employee;
+import com.app.employee.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employee")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employee) {
+        return employeeRepository.findById(id).map(
+                newemployee -> {
+                    newemployee.setName(employee.getName());
+                    newemployee.setLastname(employee.getLastname());
+                    newemployee.setEmail(employee.getEmail());
+                    return ResponseEntity.ok(employeeRepository.save(newemployee));
+                }).orElse(ResponseEntity.notFound().build());
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable Long id, Locale locale) {
+        return employeeRepository.findById(id).map(employee -> {
+            employeeRepository.delete(employee);
+            String message = messageSource.getMessage("employee.deleted", null, locale);
+            return ResponseEntity.ok(message);
+        }).orElseGet(() -> {
+            String message = messageSource.getMessage("employee.notfound", null, locale);
+            return ResponseEntity.status(404).body(message);
+        });
+    }
+}
+}
